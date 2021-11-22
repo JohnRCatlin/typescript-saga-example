bd9a2eaf70693b2040be92e8d63a82c68268e882
John Catlin <johncatlin@Johns-MacBook-Air.local>
2021-11-22 17:05:07 +0000

    refactor: semantics

---
+++ src/OrderProcessorWorkflow.ts
@@ -0,0 +1,66 @@
+import 'reflect-metadata';
+import {Service} from 'typedi';
+
+import {
+  SagaBuilder,
+  SagaCompensationFailed,
+  SagaExecutionFailed,
+} from 'node-sagas';
+import {Order} from './Order';
+import {OrderProcessingEvelope} from './OrderProcessingEvelope';
+import {OrderProcessorSteps} from './FauxOrderProcessorSteps';
+
+type OrderProcessorOutcome = OrderProcessingEvelope;
+
+/**
+ * An order processing workflow.
+ */
+@Service()
+class OrderProcessorWorkflow {
+  constructor(private stepsProcessor: OrderProcessorSteps) {}
+
+  private saga = new SagaBuilder<OrderProcessingEvelope>()
+    .step('Create order')
+    .invoke(async (order: OrderProcessingEvelope) => {
+      await this.stepsProcessor.placeOrder(order);
+    })
+    .withCompensation(async (order: OrderProcessingEvelope) => {
+      await this.stepsProcessor.undoPlaceOrder(order);
+    })
+
+    .step('Reserve credit')
+    .invoke(async (order: OrderProcessingEvelope) => {
+      await this.stepsProcessor.reserveCredit(order);
+    })
+    .withCompensation(async (order: OrderProcessingEvelope) => {
+      await this.stepsProcessor.undoReserveCredit(order);
+    })
+
+    .step('Approve order')
+    .invoke(async (order: OrderProcessingEvelope) => {
+      await this.stepsProcessor.approveOrder(order);
+    })
+    .build();
+
+  /**
+   * Process the order.
+   * @returns
+   */
+  async execute(order: Order): Promise<OrderProcessorOutcome> {
+    const envelope = new OrderProcessingEvelope(order);
+    await this.saga.execute(envelope).catch(e => {
+      if (e instanceof SagaExecutionFailed) {
+        //processing failure.
+        //revert success.
+        console.log(e.message);
+      } else if (e instanceof SagaCompensationFailed) {
+        //processing failure.
+        //revert failure.
+        console.log(e.message);
+      }
+    });
+    return envelope;
+  }
+}
+
+export {OrderProcessorWorkflow, OrderProcessorOutcome};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import 'reflect-metadata';
 import {Container} from 'typedi';
-import {OrderProcessor, OrderProcessorOutcome} from './OrderProcessor';
+import {
+  OrderProcessorWorkflow,
+  OrderProcessorOutcome,
+} from './OrderProcessorWorkflow';
 import {Order} from './Order';
 
 /**
@@ -8,8 +11,8 @@ import {Order} from './Order';
  */
 async function main() {
   const order: Order = new Order(123, 456);
-  const processor = Container.get(OrderProcessor);
-  const outcome: OrderProcessorOutcome = await processor.execute(order);
+  const workflow = Container.get(OrderProcessorWorkflow);
+  const outcome: OrderProcessorOutcome = await workflow.execute(order);
   console.log(`outcome = ${JSON.stringify(outcome, null, 2)}`);
 }
 
